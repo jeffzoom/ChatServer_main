43116b3e6f59b3cdeccd53ed747b35d1012cf5df
jeffzoom <111035313+jeffzoom@users.noreply.github.com>
2024-10-03 11:32:38 +0800

    Add files via upload

---
+++ mprpc/build/src/client/CMakeFiles/ChatClient.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/client/main.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/client/CMakeFiles/ChatClient.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../include/server/mymprpc"
+  "../include/server/rpc_proto_h"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mprpc/build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/db/CommonConnectionPool.cpp.o"
+  "CMakeFiles/ChatServer.dir/db/Connection.cpp.o"
+  "CMakeFiles/ChatServer.dir/db/db_chat.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  "CMakeFiles/ChatServer.dir/rpc_proto_cc/friendmodel.pb.cc.o"
+  "CMakeFiles/ChatServer.dir/rpc_proto_cc/groupmodel.pb.cc.o"
+  "CMakeFiles/ChatServer.dir/rpc_proto_cc/offlinemessagemodel.pb.cc.o"
+  "CMakeFiles/ChatServer.dir/rpc_proto_cc/usermodel.pb.cc.o"
+  "../../../bin/ChatServer.pdb"
+  "../../../bin/ChatServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
