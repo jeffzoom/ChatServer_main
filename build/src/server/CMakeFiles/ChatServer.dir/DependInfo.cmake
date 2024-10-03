199f035e743db4fd3df0105ff76d45e2adbd7795
jeffzoom <111035313+jeffzoom@users.noreply.github.com>
2024-10-03 11:14:38 +0800

    Add files via upload

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/chatserver.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/chatservice.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/db/CommonConnectionPool.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/CommonConnectionPool.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/db/Connection.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/Connection.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/db/db_chat.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db_chat.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/main.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/model/friendmodel.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/model/groupmodel.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/model/offlinemessagemodel.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/model/usermodel.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/redis/redis.cpp" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/rpc_proto_cc/friendmodel.pb.cc" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/rpc_proto_cc/friendmodel.pb.cc.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/rpc_proto_cc/groupmodel.pb.cc" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/rpc_proto_cc/groupmodel.pb.cc.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/rpc_proto_cc/offlinemessagemodel.pb.cc" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/rpc_proto_cc/offlinemessagemodel.pb.cc.o"
+  "/home/nowcoder/Linux_nc/ChatServer/myChatServer/src/server/rpc_proto_cc/usermodel.pb.cc" "/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/CMakeFiles/ChatServer.dir/rpc_proto_cc/usermodel.pb.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../include/server/mymprpc"
+  "../include/server/rpc_proto_h"
+  "../thirdparty"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
